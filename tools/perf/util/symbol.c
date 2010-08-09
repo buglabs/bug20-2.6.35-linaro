781e2440e1c22831023f59675ed89265a4f5e781
Dave Martin <dave.martin@linaro.org>
2010-08-09 12:21:18 +0100

    perf symbols: Ignore mapping symbols on ARM
    ARM ELF files use symbols with special names $a, $t, $d to identify regions of
    ARM code, Thumb code and data within code sections.  This can cause confusing
    output from the perf tools, especially for partially stripped binaries, or
    binaries containing user-added zero-sized symbols (which may occur in
    hand-written assembler which hasn't been fully annotated with .size
    directives).
    
    This patch filters out these symbols at load time.
    
    LKML-Reference: <1281352878-8735-2-git-send-email-dave.martin@linaro.org>
    Signed-off-by: Dave Martin <dave.martin@linaro.org>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -1031,6 +1031,18 @@ static int dso__load_sym(struct dso *self, struct map *map, const char *name,
 		if (!is_label && !elf_sym__is_a(&sym, map->type))
 			continue;
 
+		/*
+		 * Reject ARM ELF "mapping symbols": these aren't unique and
+		 * don't identify functions, so will confuse the profile
+		 * output:
+		 */
+		if (ehdr.e_machine == EM_ARM) {
+			if (!strcmp(elf_name, "$a") ||
+			    !strcmp(elf_name, "$d") ||
+			    !strcmp(elf_name, "$t"))
+				continue;
+		}
+
 		sec = elf_getscn(elf, sym.st_shndx);
 		if (!sec)
 			goto out_elf_end;
