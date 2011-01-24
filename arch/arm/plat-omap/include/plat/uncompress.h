3f60365d906c74fddcfd47b0a36184a80fae59fb
Matt Isaacs <izzy@buglabs.net>
2011-01-24 13:47:49 -0500

    Bug Labs base low level debug and machine identification fixes...
    Signed-off-by: Matt Isaacs <izzy@buglabs.net>

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -150,6 +150,7 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 		DEBUG_LL_OMAP3(3, omap_ldp);
 		DEBUG_LL_OMAP3(3, overo);
 		DEBUG_LL_OMAP3(3, touchbook);
+		DEBUG_LL_OMAP3(3, bug20);		
 
 		/* omap4 based boards using UART3 */
 		DEBUG_LL_OMAP4(3, omap_4430sdp);
