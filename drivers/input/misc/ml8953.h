daa31e78c1abcc19c378adda130adf166076da25
Matt Isaacs <izzy@buglabs.net>
2011-03-25 16:15:25 -0400

    GPL Headers added to ML8953 Acceleromter drivers..
    Signed-off-by: Matt Isaacs <izzy@buglabs.net>

--- drivers/input/misc/ml8953.h
+++ drivers/input/misc/ml8953.h
@@ -1,3 +1,24 @@
+/**
+ * ml8953.h - OKI ML8953 Accelerometer driver
+ *
+ * Copyright (C) 2010 Bug Labs Inc.
+ *      Matt Isaacs <izzy@buglabs.net>
+ *
+ * Based an Analog Devices adxl34x accelerometer drivers.
+ *
+ * This file is subject to the terms and conditions of the GNU General
+ * Public License. See the file "COPYING" in the main directory of this
+ * archive for more details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 // I2C ACC register addresses - OKI
 #define ACC_PAGESEL			0x1E	// device ready status
