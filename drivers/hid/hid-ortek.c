cf35003d33fe0c9bc0d833e7f5172f717226999a
John E. Connolly <jconnolly@buglabs.net>
2011-08-12 13:23:27 -0400

    cleanup of Ortek commit for 2.6.35

--- drivers/hid/hid-ortek.c
+++ drivers/hid/hid-ortek.c
@@ -28,10 +28,10 @@ static void ortek_report_fixup(struct hid_device *hdev, __u8 *rdesc,
 		unsigned int rsize)
 {
 	if (rsize >= 56 && rdesc[54] == 0x25 && rdesc[55] == 0x01) {
-		hid_info(hdev, "Fixing up logical minimum in report descriptor (Ortek)\n");
+                dev_info(hdev, "Fixing up logical minimum in report descriptor (Ortek)\n");
 		rdesc[55] = 0x92;
-	} else if (*rsize >= 54 && rdesc[52] == 0x25 && rdesc[53] == 0x01) {
-		hid_info(hdev, "Fixing up logical minimum in report descriptor (Skycable)\n");
+        } else if (rsize >= 54 && rdesc[52] == 0x25 && rdesc[53] == 0x01) {
+                dev_info(hdev, "Fixing up logical minimum in report descriptor (Skycable)\n");
 		rdesc[53] = 0x65;
 	}
 }
